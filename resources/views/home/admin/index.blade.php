7103ce20bdd1a1bf1d8f2c204b4c6200b940ae9e
Samer Lahlouh <samer.lahlouh@gmail.com>
2019-03-27 15:49:25 +0300

    Testing

--- resources/views/home/admin/index.blade.php
+++ resources/views/home/admin/index.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 <div id="content_div">
+<?php
+    begin_incubated_child_card();
+        begin_child_card('card_checking_orders', __('home_lng.checking_orders'), 'show');
+        include('home\admin\checking_orders');
+        ?>
+        <?php end_child_card();
 
+        begin_child_card('card_checking_transfers', __('home_lng.checking_transfers'), 'show');
+        include('home\admin\checking_transfers');?>
+        <?php end_child_card();
+
+        begin_child_card('card_chargings', __('home_lng.chargings'), 'show');
+        include('home\admin\chargings');?>
+        <?php end_child_card();
+    end_incubated_child_card();
+    
+?>
 </div>
 @endsection
