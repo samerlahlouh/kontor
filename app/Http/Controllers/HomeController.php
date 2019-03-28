89b564b3ddf7e55ac71cd3c29893596239bd724e
Samer Lahlouh <samer.lahlouh@gmail.com>
2019-03-28 09:44:20 +0300

    Test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -142,7 +142,7 @@ class HomeController extends Controller
         $select_operators = $this->getEnumValues('packets', 'operator');
         $select_types = $this->getEnumValues('packets', 'type');
 
-        return view('home\regular\regular_home', ['checking_orders'     => $checking_orders,
+        return view('home.regular.regular_home', ['checking_orders'     => $checking_orders,
                                                 'checking_orders_cols'  => $checking_orders_cols,
                                                 'checked_orders'        => $checked_orders,
                                                 'checked_orders_cols'   => $checked_orders_cols,
